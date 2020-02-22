ec534cdc330dee531ac6d4153bf13c9f903eb128
Mona Lisa <honodan@daymail.life>
2020-02-22 11:46:06 +0300

    Initial Commit

---
+++ TreasureDiver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TreasureDiver
+//
+//  Created by Jethro on 2/22/20.
+//  Copyright © 2020 Finley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
